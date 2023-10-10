e4d111b25bd55e164cb8895ec85adc9d1c5696c2
AHMAD RAHMAN <147287219+AkhmadRahman@users.noreply.github.com>
2023-10-10 12:12:57 +0800

    Days 2
    Pada Hari sebelumnya saya membuat program untuk menampilkan hello world!,pada hari ini saya membuat program untuk mengenalkan variabel umum yang digunakan dalam bahasa pemprograman java,maaf kalau kurang

---
+++ Variable_umum.java
@@ -0,0 +1,19 @@
+public class variabel_umum_java {
+    public static void main(String[] args) {
+        // Variabel umum
+        int umur = 18; //bilangan bulat (integer)
+        double  Tinggi = 176.2; //bilangan pecahan (floating-point)
+        String nama = "Ahmad Rahman"; //teks
+        boolean Ganteng = false; //hanya berisi true or false
+        char jk = 'L';
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur + " tahun");
+        System.out.println("Tinggi Badan: " + Tinggi + " Cm");
+        System.out.println("Apakah Saya Ganteng: " + Ganteng);
+        System.out.println("Jenis Kelamin: " + jk);
+    }
+}
+
+
+    
