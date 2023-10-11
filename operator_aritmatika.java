274240ef36f04d95be822810a287f19eb6bf4bb7
AHMAD RAHMAN <147287219+AkhmadRahman@users.noreply.github.com>
2023-10-11 10:31:05 +0800

    day 2
    hari ini saya membuat program operator aritmatika,
    selanjutnya operator perbandingan

--- operator_aritmatika.java
+++ operator_aritmatika.java
@@ -3,23 +3,18 @@ public class operator_aritmatika {
         int angka1 = 18;
         int angka2 = 29;
 
-        // Penjumlahan
         int Penjumlahan = angka1 + angka2;
         System.out.println("Penjumlahan: " + Penjumlahan);
-
-        // Pengurangan
+        
         int Pengurangan = angka1 - angka2;
         System.out.println("Pengurangan: " + Pengurangan);
 
-        // Perkalian
         int Perkalian = angka1 * angka2;
         System.out.println("Perkalian: " + Perkalian);
-
-        // Pembagian
+        
         int Pembagian = angka1 / angka2;
         System.out.println("Pembagian: " + Pembagian);
 
-        // Modulus
         int sisaBagi = angka1 % angka2;
         System.out.println("Sisa hasil bagi: " + sisaBagi);
     }
