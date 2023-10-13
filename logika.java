a706a92950cae35fe517821ba05a3dc3abed745b
AHMAD RAHMAN <147287219+AkhmadRahman@users.noreply.github.com>
2023-10-13 09:41:20 +0800

    day5
    hari ini operator logika
    selanjutnya operator penugasan

--- logika.java
+++ logika.java
@@ -3,13 +3,13 @@ public class logika {
         boolean a = true;
         boolean b = false;
 
-        boolean hasilAND = a && b; //menghasilkan true hanya jika keduanya bernilai true.
+        boolean hasilAND = a && b; //menghasilkan true kalau keduanya bernilai true.
         System.out.println("Hasil AND: " + hasilAND);
 
         boolean hasilOR = a || b; //menghasilkan true jika salah satu atau keduanya bernilai true.
         System.out.println("Hasil OR: " + hasilOR);
 
-        boolean hasilNOT = !a; //digunakan untuk membalikkan nilai, Jika awalnya true, hasilnya menjadi false, dan sebaliknya.
+        boolean hasilNOT = !a; //digunakan untuk membalikkan nilai, Jika awalnya true, jadi false, dan sebaliknya.
         System.out.println("Hasil NOT: " + hasilNOT);
     }
 }
