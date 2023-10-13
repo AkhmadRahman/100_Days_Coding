49a16fe7a0ccd2ee6efea7c47f964b025f198f3b
AHMAD RAHMAN <147287219+AkhmadRahman@users.noreply.github.com>
2023-10-14 07:59:03 +0800

    Day 6
    hari ini tidak jadi buat operator penugasan,lupa caranya,takutnya disuruh jelaskan ngang ngong ngang ngong

---
+++ percabangan.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication10;
+
+/**
+ *
+ * @author USER
+ */
+public class percabangan {
+    public static void main(String[] args) {
+        int angka = 100;  //variabel angka 100
+        if (angka > 99) { //jika angka lebih dari 99
+            System.out.println("lebih besar"); //ditampilkan jika lebih besar
+        } else { //jika tidak
+            System.out.println("Lebih Kecil Itumah"); //jika lebih kecil
+        }
+    }
+}
+
