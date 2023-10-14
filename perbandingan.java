6a94f3a410e880d9584a076e8d189665caa194c5
AHMAD RAHMAN <147287219+AkhmadRahman@users.noreply.github.com>
2023-10-14 09:06:42 +0800

    Day4
    Hari keempat 
    pengenalan operator perbandingan

--- perbandingan.java
+++ perbandingan.java
@@ -3,9 +3,9 @@ public class operatorperbandingan {
         int umur_ari = 5;
         int umur_yanto = 10;
 
-        boolean lebihbesar = umur_ari > umur_yanto;
-        boolean lebihkecil = umur_ari < umur_yanto;
-        boolean samadengan = umur_ari == umur_yanto;
+        boolean lebihbesar = umur_ari > umur_yanto; //lebih besar yoi
+        boolean lebihkecil = umur_ari < umur_yanto; //lebih kecil dari
+        boolean samadengan = umur_ari == umur_yanto; //sama dengan
         boolean tidaksama = umur_ari != umur_yanto; //tidak sama dengan
         boolean besarsamadengan = umur_ari >= umur_yanto; //lebih besar atau samadengan
         boolean kecilsamadengan = umur_ari <= umur_yanto; //lebih kecilr atau samadengan
