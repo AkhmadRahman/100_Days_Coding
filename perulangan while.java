1ba46482e64837b5453599de1882c1c9426138cd
AHMAD RAHMAN <147287219+AkhmadRahman@users.noreply.github.com>
2023-10-17 20:08:18 +0800

    day 9
    saya membuat perulangan menggunakan while,jadi akan terus menampilkan angka samapi dengan samadengan 100

---
+++ perulangan while.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class NewClass1 {
+    public static void main(String[] args) {
+        int angka = 1;
+while (angka <= 100) {
+    System.out.println("angka:" + angka);
+    angka++;
+}
+}
+}
+
