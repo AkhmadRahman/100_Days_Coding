bff53e0115fb67abb782ce01d0ecfe72874bc370
AHMAD RAHMAN <147287219+AkhmadRahman@users.noreply.github.com>
2023-10-18 18:43:01 +0800

    Day 10
    beda while dan do while ketika while mengecek kondisi dulu kemudian menjalankan kode jika kondisi benar, sedangkan do while menjalankan kode sebelum mengecek kondisi.

---
+++ do_while.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class do_while {
+    public static void main(String[] args) {
+        int Nomor = 1;
+            do {
+                System.out.println("Nomor:" + Nomor);
+                Nomor++;
+                }
+            while (Nomor <= 100);
+    }
+}
