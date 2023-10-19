31216d12abe31d1cb515c68d1ad4f057b1e12e31
AHMAD RAHMAN <147287219+AkhmadRahman@users.noreply.github.com>
2023-10-19 19:48:02 +0800

    Day 11
    terdapat variabel dengan nama pilihan yaitu 200
    disini menggunkan switch bukan if  yang mana pilihan akan dijalankan adalah pilihan pada variabel pilihan
    Case artinya kasus dalam artian pilihan pertama dan seterusnya

---
+++ percabangan_switch.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication10;
+
+/**
+ *
+ * @author USER
+ */
+public class Percabangan_switch {
+    public static void main(String[] args) {
+        int pilihan = 200;
+    switch (pilihan) {
+        case 100:
+            System.out.println("Kamu Memilih Pilihan pertama Yaitu 100");
+            break;
+        case 200:
+            System.out.println("Kamu Memilih Pilihan Kedua Yaitu 200");
+            break;
+        default:
+            System.out.println("Tidak Ada yang terpilih");
+        }
+    }
+}
